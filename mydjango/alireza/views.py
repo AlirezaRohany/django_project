ed8e9c399b87f417e907daa38ce266ab3ea97b8c
Alireza Rohany <alireza.rohany.77@gmail.com>
2020-09-14 23:51:17 +0430

    used generic views system and removing redundancy

--- mydjango/alireza/views.py
+++ mydjango/alireza/views.py
@@ -1,25 +1,44 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse,HttpResponseRedirect
-from .models import Question,Choice
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'alireza/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'alireza/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'alireza/detail.html', {'question': question})
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'alireza/index.html', context)
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'alireza/detail.html'
+
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'alireza/detail.html', {'question': question})
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'alireza/results.html'
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'alireza/results.html', {'question': question})
 
 
 def vote(request, question_id):
