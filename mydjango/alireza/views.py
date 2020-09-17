c2c4ea3fe55eb579fe3a745b30f0a0044a4bf3eb
Alireza Rohany <alireza.rohany.77@gmail.com>
2020-09-17 17:33:50 +0430

    more automated tests for "Question" model in detail.html and index.html

--- mydjango/alireza/views.py
+++ mydjango/alireza/views.py
@@ -5,6 +5,7 @@ from .models import Question, Choice
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 
 class IndexView(generic.ListView):
@@ -12,7 +13,9 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        # """Return the last five published questions."""
+        # return Question.objects.order_by('-pub_date')
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 # def index(request):
@@ -24,6 +27,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'alireza/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 # def detail(request, question_id):
